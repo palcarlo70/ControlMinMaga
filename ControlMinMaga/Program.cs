44e1a634871d345e3ecf34f1e8fb414809d2489c
Carlo <PalCarlo@hotmail.com>
2021-03-25 17:06:37 +0100

    bvhncgh

--- ControlMinMaga/Program.cs
+++ ControlMinMaga/Program.cs
@@ -4,14 +4,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace ControlMinMaga
 {
     public class Program
     {
+        private System.Timers.Timer timer;
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            Timer newTimer = new Timer();
+            newTimer.Elapsed += new ElapsedEventHandler(DisplayTimeEvent);
+            newTimer.Interval = 2000;
+            newTimer.Start();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -20,5 +27,10 @@ namespace ControlMinMaga
                 {
                     services.AddHostedService<Worker>();
                 });
+
+        public static void DisplayTimeEvent(object source, ElapsedEventArgs e)
+        {
+            Console.Write(" \r{0} ", DateTime.Now);
+        }
     }
 }
